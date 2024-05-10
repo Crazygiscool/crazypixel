9239e39895c9c74b588446e7c2bd748170378774
aCrazyg1 <huxiaodou768@hotmail.com>
2024-05-10 19:12:47 -0400

    preset from the "Everything" v1.0-SNAPSHOT plugin

---
+++ src/main/java/me/crazyg/crazypixel/Crazypixel.java
@@ -0,0 +1,47 @@
+package me.crazyg.crazypixel;
+
+import me.crazyg.crazypixel.Commands.GodCommand;
+import me.crazyg.crazypixel.Commands.KillCommand;
+import me.crazyg.crazypixel.Commands.MenuCommand;
+import me.crazyg.crazypixel.Commands.RepeatCommand;
+import me.crazyg.crazypixel.Listeners.onJoinleaveListener;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+
+public final class Crazypixel extends JavaPlugin {
+    private Logger log = this.getLogger();
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        log.info("Plugin Made By "+ ChatColor.DARK_AQUA+"Crazyg");
+        log.info("Thanks for using the plugin");
+        log.info("\n" +
+                "░█████╗░██████╗░░█████╗░███████╗██╗░░░██╗██████╗░██╗██╗░░██╗███████╗██╗░░░░░\n" +
+                "██╔══██╗██╔══██╗██╔══██╗╚════██║╚██╗░██╔╝██╔══██╗██║╚██╗██╔╝██╔════╝██║░░░░░\n" +
+                "██║░░╚═╝██████╔╝███████║░░███╔═╝░╚████╔╝░██████╔╝██║░╚███╔╝░█████╗░░██║░░░░░\n" +
+                "██║░░██╗██╔══██╗██╔══██║██╔══╝░░░░╚██╔╝░░██╔═══╝░██║░██╔██╗░██╔══╝░░██║░░░░░\n" +
+                "╚█████╔╝██║░░██║██║░░██║███████╗░░░██║░░░██║░░░░░██║██╔╝╚██╗███████╗███████╗\n" +
+                "░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝╚══════╝");
+        // Commands
+        getCommand("kill").setExecutor(new KillCommand());
+        getCommand("god").setExecutor(new GodCommand());
+        getCommand("repeat").setExecutor(new RepeatCommand());
+        getCommand("menu").setExecutor(new MenuCommand());
+        // Listeners
+        getServer().getPluginManager().registerEvents((Listener) new onJoinleaveListener(this), (Plugin) this);
+        //config.yml
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        log.info("CYA!!!!");
+    }
+}
---
+++ src/main/java/me/crazyg/crazypixel/Listeners/onJoinleaveListener.java
@@ -0,0 +1,73 @@
+package me.crazyg.crazypixel.Listeners;
+
+import me.crazyg.crazypixel.Crazypixel;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import java.util.logging.Logger;
+
+public class onJoinleaveListener implements Listener {
+
+    private Logger log = this.getLogger();
+
+    private Logger getLogger() {
+        return null;
+    }
+
+    private final Crazypixel plugin;
+
+    public onJoinleaveListener(Crazypixel plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e){
+
+        Player player = e.getPlayer();
+
+        String leavemsg = this.plugin.getConfig().getString("leave-message");
+
+        if(leavemsg != null){
+            leavemsg = leavemsg.replace("%player%", e.getPlayer().getDisplayName());
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', leavemsg));
+
+        }else{
+            log.info("SET THE LEAVE-MESSAGE IN CONFIG.YML");
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+
+        Player player = e.getPlayer();
+
+        String joinmsg = this.plugin.getConfig().getString("join-message");
+
+        String firstjoinmsg = this.plugin.getConfig().getString("first-join-message");
+        if (player.hasPlayedBefore()){
+
+            if(joinmsg != null){
+                joinmsg = joinmsg.replace("%player%", e.getPlayer().getDisplayName());
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', joinmsg));
+
+            }else{
+                log.info("SET THE JOIN-MESSAGE IN CONFIG.YML");
+            }
+
+        }else{
+
+            if(firstjoinmsg != null){
+                firstjoinmsg = firstjoinmsg.replace("%player%", e.getPlayer().getDisplayName());
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', firstjoinmsg));
+
+            }else{
+                log.info("SET THE FIRST-JOIN-MESSAGE IN CONFIG.YML");
+            }
+
+        }
+
+    }
+}
